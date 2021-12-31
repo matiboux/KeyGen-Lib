f2e91e6c7b660b8f63ffc9b7a3e1125364069d03
Matiboux <matiboux@gmail.com>
2021-12-31 15:05:26 +0100

    Add magic static method to access KeyGen methods

--- KeyGen-Lib/KeyGenStatic.php
+++ KeyGen-Lib/KeyGenStatic.php
@@ -33,10 +33,26 @@
 namespace KeyGenLib;
 
 /**
+ * @method static int getFlags()
+ * @method static void setParams(?int $length = null, ?bool $numeric = null, ?bool $lowercase = null, ?bool $uppercase = null, ?bool $special = null, ?bool $redundancy = null)
+ * @method static void updateParams(?int $length = null, ?bool $numeric = null, ?bool $lowercase = null, ?bool $uppercase = null, ?bool $special = null, ?bool $redundancy = null)
+ * @method static void resetParams()
+ * @method static mixed[] getParams()
+ * @method static bool isDefaultParameters()
+ * @method static ?string keygen(?int $length = null, ?bool $numeric = null, ?bool $lowercase = null, ?bool $uppercase = null, ?bool $special = null, ?bool $redundancy = null)
+ * @method static bool isForcedRedundancy()
+ * @method static bool isError()
+ * @method static ?array getErrorInfos()
+ * @method static int getErrorId()
+ * @method static ?string getErrorCode()
+ * @method static ?string getErrorMessage()
+ *
  * @author Matiboux <matiboux@gmail.com>
  */
 class KeyGenStatic
 {
+	#region Static context use
+
 	private static KeyGen $instance;
 
 	public static function getStaticInstance(): KeyGen
@@ -49,94 +65,16 @@ class KeyGenStatic
 		return self::$instance;
 	}
 
-	public static function getFlags(): int
-	{
-		return self::getStaticInstance()->getFlags();
-	}
-
-	public static function setParams(
-		?int $length = null,
-		?bool $numeric = null,
-		?bool $lowercase = null,
-		?bool $uppercase = null,
-		?bool $special = null,
-		?bool $redundancy = null,
-	): void {
-		self::getStaticInstance()->setParams($length, $numeric, $lowercase, $uppercase, $special, $redundancy);
-	}
-
-	public static function updateParams(
-		?int $length = null,
-		?bool $numeric = null,
-		?bool $lowercase = null,
-		?bool $uppercase = null,
-		?bool $special = null,
-		?bool $redundancy = null,
-	): void {
-		self::getStaticInstance()->updateParams($length, $numeric, $lowercase, $uppercase, $special, $redundancy);
-	}
-
-	public static function resetParams(): void
-	{
-		self::getStaticInstance()->resetParams();
-	}
-
 	/**
-	 * @return mixed[]
+	 * @param string $name
+	 * @param mixed[] $arguments
+	 * @return mixed
 	 */
-	public static function getParams(): array
-	{
-		return self::getStaticInstance()->getParams();
-	}
+    public static function __callStatic(string $name, array $arguments): mixed
+    {
+		$instance = self::getStaticInstance();
+		return $instance->$name(...$arguments);
+    }
 
-	public static function isDefaultParameters(): bool
-	{
-		return self::getStaticInstance()->isDefaultParameters();
-	}
-
-	public static function keygen(
-		?int $length = null,
-		?bool $numeric = null,
-		?bool $lowercase = null,
-		?bool $uppercase = null,
-		?bool $special = null,
-		?bool $redundancy = null
-	): ?string {
-		return self::getStaticInstance()->keygen($length, $numeric, $lowercase, $uppercase, $special, $redundancy);
-	}
-
-	public static function isForcedRedundancy(): bool
-	{
-		return self::getStaticInstance()->isForcedRedundancy();
-	}
-
-	public static function isError(): bool
-	{
-		return self::getStaticInstance()->isError();
-	}
-
-	/**
-	 * Get Error Infos.
-	 *
-	 * @return mixed[]|null
-	 */
-	public static function getErrorInfos(): array|null
-	{
-		return self::getStaticInstance()->getErrorInfos();
-	}
-
-	public static function getErrorId(): int
-	{
-		return self::getStaticInstance()->getErrorId();
-	}
-
-	public static function getErrorCode(): ?string
-	{
-		return self::getStaticInstance()->getErrorCode();
-	}
-
-	public static function getErrorMessage(): ?string
-	{
-		return self::getStaticInstance()->getErrorMessage();
-	}
+	#endregion
 }
